9a0c82b809d9d93c0376223fc364eae4e2af4d89
Azat Ismagilov <azatismagilov00@gmail.com>
2023-09-12 00:17:43 +0100

    Use rsync to sync files

--- instance/start.sh
+++ instance/start.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Iterate over all files in /app
-for file in /app/*; do
-    # Check if the file exists in /workspace
-    if [ ! -f "/workspace/$(basename "$file")" ]; then
-        # Copy the file from /app to /workspace
-        cp "$file" "/workspace/$(basename "$file")"
-    fi
-done
+rsync -av --ignore-existing /app/ /workspace/
 
-# Execute the main command, in this case, supervisord
 exec /usr/bin/supervisord
